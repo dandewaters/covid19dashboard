1f507901fad1a146ad15c5fc89670e61414f30c3
Daniel DeWaters <42219981+dandewaters@users.noreply.github.com>
2023-12-29 15:45:40 -0500

    Removed package stuff and old data retrieval scripts

--- R/other/setup.R
+++ R/other/setup.R
@@ -1,20 +1,27 @@
 library(shiny)
+library(shinyWidgets)
+library(shinydashboard)
+library(shinyalert)
 library(plotly)
+library(fontawesome)
 library(tidyverse)
 library(lubridate)
 library(data.table)
 library(purrr)
 
+
 # Helper functions
 
 
 # Execute Data Retrieval
-source("./data_retrieval/make_data_file_name.R")
-source("./data_retrieval/get_covid_case_data.R")
-source("./data_retrieval/get_other_covid_vaccine_data.R")
-source("./data_retrieval/get_data.R")
+case_data <- read_rds('./data/case_data_2023_12_29.rds')
+vaccine_data <- read_rds('./data/vaccine_data_2023_12_29.rds')
+# source("./R/data_retrieval/make_data_file_name.R")
+# source("./R/data_retrieval/get_covid_case_data.R")
+# source("./R/data_retrieval/get_other_covid_vaccine_data.R")
+# source("./R/data_retrieval/get_data.R")
 
 # Define plot functions
-source("./plots/new_covid_cases_plot.R")
-source("./plots/covid_log_plot.R")
-source("./plots/other_covid_vaccines_plot.R")
+source("./R/plots/new_covid_cases_plot.R")
+source("./R/plots/covid_log_plot.R")
+source("./R/plots/other_covid_vaccines_plot.R")
